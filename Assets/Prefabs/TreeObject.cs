b07651d692313d9dcbf980dd1333bddb3cbaa2eb
kamiljot <kamiljotzia@gmail.com>
2020-12-22 22:27:18 +0100

    add tree class
    add tree class

---
+++ Assets/Prefabs/TreeObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject
+{
+    private TreeClr _treeColor;
+    private TreeLvl _treeLevel;
+
+    public enum TreeClr
+    {
+        GREEN,
+        RED,
+        YELLOW,
+        BLUE
+    }
+
+    public enum TreeLvl
+    {
+        SEED,
+        SMALL,
+        MID,
+        BIG
+    }
+
+    public TreeLvl TreeLevel
+    {
+        get
+        {
+            return _treeLevel;
+        }
+        set
+        {
+            _treeLevel = value;
+        }
+    }
+
+    public TreeClr TreeColor
+    {
+        get
+        {
+            return _treeColor;
+        }
+        set
+        {
+            _treeColor = value;
+        }
+    }
+    public void lvlUp()
+    {
+        if ((int)_treeColor < 3)
+        {
+            _treeColor += 1;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
