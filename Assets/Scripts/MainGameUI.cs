da94e0a046e178183819991e9fa14b28bfd43e0a
wazniak96 <wazniak96@gmail.com>
2021-01-01 15:51:41 +0100

    Fixing screen resolution of main game UI. Improving game menu.

---
+++ Assets/Scripts/MainGameUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainGameUI : MonoBehaviour
+{
+
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene("_GAME_MENU_SCENE");
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     public void NewGame()
     {
         SceneManager.LoadScene("_MAIN_SCENE");
+    }
+
+    public void BackToGame()
+    {
+        SceneManager.LoadScene("_MAIN_SCENE");
+    }
+
+    public void OpenTutorial()
+    {
+        System.Diagnostics.Process.Start("https://portalgames.blob.core.windows.net/fotosynteza/Fotosynteza-Instrukcja.pdf");
     }
 
     public void QuitGame()
