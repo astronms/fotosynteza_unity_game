50cb3b6625205d6f9270683c71baaba050ab938b
kamiljot <kamiljotzia@gmail.com>
2021-01-11 19:33:36 +0100

    no message

---
+++ Assets/Scripts/Tree/TreeObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour
+{
+    private TreeClr _treeColor { get; set; }
+    private TreeLvl _treeLevel { get; set; }
+
+    public enum TreeClr
+    {
+        GREEN,
+        RED,
+        YELLOW,
+        BLUE
+    }
+
+    public enum TreeLvl
+    {
+        SEED,
+        SMALL,
+        MID,
+        BIG
+    }
+
+    public void lvlUp()
+    {
+        if ((int)_treeColor < 3)
+        {
+            _treeColor += 1;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
